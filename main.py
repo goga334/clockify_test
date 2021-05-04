ccabbf668642475b5c41c9bc1a2c2cd376bc573f
goga334 <53278040+goga334@users.noreply.github.com>
2021-05-04 21:15:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import requests
 import configparser
 
@@ -17,24 +16,3 @@ except requests.exceptions.RequestException as e:
 else:
     json_response_base = response.json()
     print("Records received successfully!")
-#
-# workspaceId = json_response_base['activeWorkspace']
-# userId = json_response_base['id']
-#
-# start_utc = '2020' + '-' + '01' + '-' + '01' + 'T00:00:00Z'
-# params={'start': start_utc, 'page-size' : 1000}
-# api_time_entries = f'/workspaces/{workspaceId}/user/{userId}/time-entries'
-# api_url = url + api_time_entries
-# response = requests.get(api_url, headers=headers, params=params)
-# json_response_te = response.json()
-#
-# df = pd.DataFrame.from_dict(json_response_te)
-# df = df[['description','billable','timeInterval']]
-# df = pd.concat([df.drop('timeInterval', axis=1), pd.DataFrame(df['timeInterval'].values.tolist())], axis=1)
-# df['start'] = pd.to_datetime(df['start'])
-# df['end'] = pd.to_datetime(df['end'])
-# df['duration'] = df['end'] - df['start']
-# df.fillna('', inplace=True)
-#
-# for i in range(df.shape[0]):
-#     print(df.loc[i], "\n\n")
